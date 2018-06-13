2744dc3320c6eb8e541df43f52987537543c82a3
Girish <girish12ramesh@gmail.com>
2018-06-13 10:19:05 -0700

    First Commit

---
+++ app/src/main/java/com/example/girish/contacts/AddTaskViewModel.java
@@ -0,0 +1,28 @@
+package com.example.girish.contacts;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.girish.contacts.database.AppDatabase;
+import com.example.girish.contacts.database.TaskEntry;
+
+//import com.example.android.todolist.database.AppDatabase;
+//import com.example.android.todolist.database.TaskEntry;
+
+// COMPLETED (5) Make this class extend ViewModel
+public class AddTaskViewModel extends ViewModel {
+
+    // COMPLETED (6) Add a task member variable for the TaskEntry object wrapped in a LiveData
+    private LiveData<TaskEntry> task;
+
+    // COMPLETED (8) Create a constructor where you call loadTaskById of the taskDao to initialize the tasks variable
+    // Note: The constructor should receive the database and the taskId
+    public AddTaskViewModel(AppDatabase database, int taskId) {
+        task = database.taskDao().loadTaskById(taskId);
+    }
+
+    // COMPLETED (7) Create a getter for the task variable
+    public LiveData<TaskEntry> getTask() {
+        return task;
+    }
+}
